0b1ca46a6a3ee2459923e99d20819a7eb2b7be27
Harley Gray <harley.gray.96@gmail.com>
2024-12-27 14:17:41 +1000

    cross origin update

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "civicus.fyi"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :civicus, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -62,7 +62,11 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: [
+      "https://www.civicus.fyi",
+      "https://civicus.fyi"
+    ]
 
   # ## SSL Support
   #
